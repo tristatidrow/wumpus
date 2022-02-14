1e382fe04b129544fd6967c6b7225bc1d1d3974d
trista tidrow <99518812+tristatidrow@users.noreply.github.com>
2022-02-14 20:45:01 +0000

    end of day one

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
+alive = True
 currentRoom = 1
 caveMap = {
   1: [2, 3, 8],
   2: [1, 3, 13],
   3: [1, 2, 5],
   4: [1, 2, 3],
-  5: [1, 6],
+  5: [1, 6, 14],
   6: [5, 9, 10],
   7: [5, 8, 9],
   8: [7, 10, 4],
   9: [6, 7, 10],
   10: [12, 6, 9, 8],
   11: [13, 8, 6],
-  12: [10, 2, 13],
-  13: [11, 4, 2]
+  12: [10, 2, 13, 14],
+  13: [11, 4, 2],
+  14: []
 }
+
+def niceExitList(roomExits):
+  global alive
+  numExits = len(roomExits)
   
+  if numExits == 0:
+    alive = False
+    return "bahaha loser ur trapped and u ded"
+  
+  if numExits == 1:
+    return "this room is special it only goes to {roomExits[0]}"
+  
+  if len(roomExits) == 2:
+    return f"this room has exits to rooms {roomExits[0]} and {roomExits[1]}"
+  
+  niceList = "this room has exit to rooms: "
+  for exitNum in range(numExits-1):
+    niceList += f"{roomExits[exitNum]}, "
+  niceList += f" and {roomExits[-1]}"
+  
+  return niceList
 
 def look():
   print(f"bestie ur in room {currentRoom}")
-  if len(caveMap[currentRoom]) == 0:
-    print("bahaha loser ur trapped")
-  print(f"exits lead to room: {caveMap[currentRoom]}")
+  print(niceExitList(caveMap[currentRoom]))
 
 def move():
   global currentRoom
@@ -36,8 +56,10 @@ def move():
 print("ur mom")
 print()
 
-while True:
+while alive:
   look()
+  if not alive: 
+    break
   nextAction = input("\nwhats next")
 
   if nextAction.lower()[0] == "m":
