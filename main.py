c27b1cda8c9679df501a67bad9cd98131490f582
trista tidrow <99518812+tristatidrow@users.noreply.github.com>
2022-02-15 17:28:53 +0000

    day 2 lunch

--- main.py
+++ main.py
@@ -1,28 +1,64 @@
-alive = True
-currentRoom = 1
-caveMap = {
-  1: [2, 3, 8],
-  2: [1, 3, 13],
-  3: [1, 2, 5],
-  4: [1, 2, 3],
-  5: [1, 6, 14],
-  6: [5, 9, 10],
-  7: [5, 8, 9],
-  8: [7, 10, 4],
-  9: [6, 7, 10],
-  10: [12, 6, 9, 8],
-  11: [13, 8, 6],
-  12: [10, 2, 13, 14],
-  13: [11, 4, 2],
-  14: []
+import random
+from enum import Enum
+
+class WumpusState(Enum):
+  DEAD = 0
+  AWAKE = 1
+  ASLEEP = 2
+
+gameState = {
+  "alive": True,
+  "wumpusState": WumpusState.ASLEEP,
+  "currentRoom": 1,
+  "wumpusRoom" : 1,
+  "caveMap": {
+    1: [2, 3, 8],
+    2: [1, 3, 13],
+    3: [1, 2, 5],
+    4: [1, 2, 3],
+    5: [1, 6, 14],
+    6: [5, 9, 10],
+    7: [5, 8, 9],
+    8: [4, 10, 7],
+    9: [6, 7, 10],
+    10: [12, 6, 9, 8],
+    11: [13, 8, 6],
+    12: [10, 2, 13, 14],
+    13: [11, 4, 2],
+    14: []
+  }
 }
 
-def niceExitList(roomExits):
-  global alive
+def numOfRooms(state):
+  count = 0
+  for room in state["caveMap"]:
+    count += 1
+  return count
+
+def safeRandomRoom(state):
+  while True:
+    room = random.randint(1, numOfRooms(state))
+    roomExits =state["caveMap"][room]
+    if len(roomExits) > 0:
+      break
+  return room      
+
+def newGame(state):
+  if numOfRooms(state) < 2:
+    print("a game that only has one room is not supported")
+    raise SystemExit
+  while state["wumpusRoom"] == state["currentRoom"]:
+    state["wumpusRoom"] = safeRandomRoom(state)
+    state["currentRoom"] = safeRandomRoom(state)
+  return
+
+def niceExitList(state):
+  currentRoom = state["currentRoom"]
+  roomExits = state["caveMap"][currentRoom]
   numExits = len(roomExits)
   
   if numExits == 0:
-    alive = False
+    state["alive"] = False
     return "bahaha loser ur trapped and u ded"
   
   if numExits == 1:
@@ -38,32 +74,50 @@ def niceExitList(roomExits):
   
   return niceList
 
-def look():
+def look(state):
+  currentRoom = state["currentRoom"]
   print(f"bestie ur in room {currentRoom}")
-  print(niceExitList(caveMap[currentRoom]))
+  if currentRoom == state["wumpusRoom"]:
+    if state["wumpusState"] == WumpusState.ASLEEP:
+      print("wumpie dumpie is taking a phat nap")
+    else:
+      print("wumpie dumpie is lookin at uuu")
+  print(niceExitList(state))
 
-def move():
-  global currentRoom
+def move(state):
+  currentRoom = state["currentRoom"]
   nextRoom = int(input("where ru going"))
-  if nextRoom not in caveMap[currentRoom]:
+  if nextRoom not in state["caveMap"][currentRoom]:
     print(f"silly goose u cant get to room {nextRoom} from here")
     return
-  if nextRoom not in caveMap:
+  if nextRoom not in state["caveMap"]:
     print(f"{nextRoom} doesnt exist smh")
     return
-  currentRoom = nextRoom
+  state["currentRoom"] = nextRoom
   
+def encounter(state):
+  if state["currentRoom"] == state["wumpusRoom"]:
+    if state["wumpusState"] == WumpusState.ASLEEP:
+      print("smh my head wumpie dumpie wants to eat u")
+      state["wumpusState"] == WumpusState.AWAKE
+    else:
+      print("nom nom nom wumpie dumpie ate u")
+      state["alive"] = False
+
+newGame(gameState)
 print("ur mom")
 print()
 
-while alive:
-  look()
-  if not alive: 
+while gameState["alive"]:
+  print("hint: the wumpus is in room {wumpusRoom}".format_map(gameState))
+  look(gameState)
+  encounter(gameState)
+  if not gameState["alive"]: 
     break
   nextAction = input("\nwhats next")
 
   if nextAction.lower()[0] == "m":
-    move()
+    move(gameState)
     continue
 
   if nextAction.lower()[0] == "q":
